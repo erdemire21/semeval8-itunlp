be0cec23f9d143be24ac61ac69cd65b7af9c4689
erdemire21 <emrehakanerdemir@gmail.com>
2025-02-24 17:06:40 +0300

    Update requirements.txt

--- pipeline.py
+++ pipeline.py
@@ -10,7 +10,6 @@ from agents import get_pandas_code
 import os
 import ast
 import pandas as pd
-#pip instal pyarrow
 
 # Get the directory of the current file
 current_file_directory = os.path.dirname(os.path.abspath(__file__))
